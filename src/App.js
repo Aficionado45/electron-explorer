2259be035621a0ac1c0a62e9b8886d3cd22d2c02
Varenya Tiwari <varenya.tiwari@iitg.ac.in>
2022-07-13 00:31:28 +0530

    OAuth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
+
 
 function App() {
+  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated } =
+    useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Auth 2.0 Authentication</p>
+        {!isAuthenticated && (
+          <button onClick={loginWithPopup}>Login with Popup</button>
+        )}
+        {!isAuthenticated && (
+          <button onClick={loginWithRedirect}>Login with Redirect</button>
+        )}
+        
+        {isAuthenticated && (
+          <pre style={{ textAlign: "center" }}>
+            {JSON.stringify(user, null, 2)}
+          </pre>
+        )}
+        {isAuthenticated && <button onClick={logout}>Logout</button>}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Auth0Provider} from '@auth0/auth0-react';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+    domain='dev-z313-qbx.us.auth0.com'
+    clientId='xrGshabZtBaDtQxkwGsx6A43A2Fa4P9Q'
+    redirectUri={window.location.origin}
+    >
+        <App />
+    </Auth0Provider>
+  
   </React.StrictMode>,
   document.getElementById('root')
 );
